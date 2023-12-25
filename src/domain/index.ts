e1e5517e998e0e84f470ac5df8a3146acedc3b4e
“Juan Diaz” <juanluisdiazguerrero@gmail.com>
2023-12-25 16:48:42 -0500

    Manejo personalizado de errores

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,2 +1,5 @@
+export * from './datasource/auth.datasource';
 export * from './dtos/auth/register-user.dto';
 export * from './entities/user.entity';
+export * from './errors/custom.error';
+export * from './repositories/Auth.respository';
