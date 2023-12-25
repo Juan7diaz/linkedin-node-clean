fc956d55d93cb80945398cf004e64f8d812aa179
“Juan Diaz” <juanluisdiazguerrero@gmail.com>
2023-12-25 02:11:10 -0500

    Implementar Register DTO

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -1,18 +1,26 @@
+import { Validators } from "../../../config";
 
 
 export class RegisterUserDto {
 
-  constructor(
-    public name:string,
-    public email:string,
-    public password:string,
-  ){}
+  private constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+  ) { }
 
+  static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
 
-  static create(object: {[key: string]: any}): [string?, RegisterUserDto?]{
+    const { name, email, password } = object
 
+    // validamos los que no viene en el body
+    if (!name) return ["missing name"];
+    if (!email) return ["missing email"];
+    if (!Validators.email.test(email)) return ["email is no valid"]
+    if (!password) return ["missing password"]
+    if (password.length < 6) return ["password too short"]
 
-    return [];
+    return [undefined, new RegisterUserDto(name, email, password)];
   }
 
 
