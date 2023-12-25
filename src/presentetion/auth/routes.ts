74398587ba3708f720875407906ea4019bfd92dd
“Juan Diaz” <juanluisdiazguerrero@gmail.com>
2023-12-25 01:51:57 -0500

    Controladores de autenticación

--- src/presentetion/auth/routes.ts
+++ src/presentetion/auth/routes.ts
@@ -1,19 +1,17 @@
 import { Router } from "express";
+import { AuthController } from "./controller";
 
 
 export class AuthRoutes {
 
   static get routes(): Router {
+
     const router = Router();
+    const controller = new AuthController()
 
     // define los metodos que va a poseer la ruta de api/auth
-    router.post("/login", (req, res)=>{
-      res.json("login")
-    })
-
-    router.post("/register", (req, res)=>{
-      res.json("register")
-    })
+    router.post("/login", controller.loginUser)
+    router.post("/register", controller.registerUser)
 
     return router
   }
